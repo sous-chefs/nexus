d543a0db089e92e9bfc4f33a54d155a93d722147
Cameron Johnston <cameron@rootdown.net>
2014-09-02 14:31:47 -0600

    enhance lwrp test coverage via nexus_test cookbook

--- test/cookbooks/nexus_test/recipes/default.rb
+++ test/cookbooks/nexus_test/recipes/default.rb
@@ -6,5 +6,19 @@
 include_recipe 'nexus'
 
 nexus_user 'testuser' do
+  email 'user@example.com'
+  password 'secret'
+  enabled true
+  roles ['wonderland']
   action :create
+end
+
+nexus_user 'testuser' do
+  email 'bob@example.com'
+  action :update
+end
+
+nexus_hosted_repository 'free_software' do
+  publisher false
+  policy 'RELEASE'
 end
